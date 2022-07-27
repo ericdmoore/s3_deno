12cd59f8be31a6f1fe240131c59c5d44f3e8ce39
Eric Moore <eric.d.moore@gmail.com>
2022-07-27 15:54:04 -0500

    no deps on code or types that escalate privs

--- deps.ts
+++ deps.ts
@@ -1,6 +1,5 @@
 
 // SIGNER is causing a priv escalation
-export type { Credentials } from "https://deno.land/x/aws_sign_v4@1.0.2/mod.ts";
 export { awsV4Sig, toAmz, toDateStamp } from 'https://denopkg.com/ericdmoore/feedBarber@main/src/lib/client/aws-url-signer.ts'
 
 export { default as parseXML } from "https://raw.githubusercontent.com/nekobato/deno-xml-parser/0bc4c2bd2f5fad36d274279978ca57eec57c680c/index.ts";
@@ -9,4 +8,10 @@ export { pooledMap } from "https://deno.land/std@0.115.1/async/pool.ts";
 
 export interface Signer{
   sign: (service: string, request: Request) => Promise<Request>
+}
+
+export interface Credentials {
+  awsAccessKeyId: string;
+  awsSecretKey: string;
+  sessionToken?: string;
 }
